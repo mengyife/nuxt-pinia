246d04a4d8468668863ab65cfb176704e6079ade
mengyife <yoengmengyi@gmail.com>
2024-10-22 08:41:52 +0000

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   ssr: true,
   devtools: { enabled: true },
+  plugins: [{ src: '~/plugins/vee-validate.client.js', mode: 'client' }],
 })
