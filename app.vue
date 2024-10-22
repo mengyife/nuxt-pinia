2722b6f36e0b8d25b593e73ec66ec36f4aeae03e
mengyife <yoengmengyi@gmail.com>
2024-10-22 06:19:43 +0000

    add code

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Counter: {{ count }}</h1>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from '~/stores/counter'
+
+const store = useCounterStore()
+const { count, increment, decrement } = store
+</script>
